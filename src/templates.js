6055829713f41fcd142b5d55b4c99db6772f6d73
gsalma99 <salmagonzalezzz10011@gmail.com>
2022-12-14 22:42:57 -0600

    added to engineer and intern templates

--- src/templates.js
+++ src/templates.js
@@ -1,3 +1,35 @@
+const InternTemplate = engineer => {
+    const name = intern.name;
+    const id = intern.employee_id;
+    const email = intern.email;
+    const school = intern.getSchool();
+    const role = intern.getRole();
+
+    return `
+
+    <h2>${role.toUpperCase()}</h2>
+    <div>Intern Name: ${name}</div>
+    <div>Intern Id: ${id}</div>
+    <div>Intern Email: ${email}</div>
+    <div>Intern Schooling: ${school}</div>
+    <div>Intern Role: ${role}</div>
+`
+}
+
 const engineerTemplate = engineer => {
-    
+    const name = engineer. name;
+    const id = engineer.employee_id;
+    const email = engineer.email;
+    const github = engineer.getGitHub();
+    const role = engineer.getRole();
+
+    return `
+
+    <h2>${role.toUpperCase()}</h2>
+    <div>Enineer Name: ${name}</div>
+    <div>Enineer Id: ${id}</div>
+    <div>Enineer Email: ${email}</div>
+    <div>Enineer Github: ${github}</div>
+    <div>Enineer Role: ${role}</div>
+`
 }
